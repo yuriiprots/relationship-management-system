9a58dcf0af7fe80a0bb6d21651a00a28e7e11951
yuriiprots <uraprots7849@gmail.com>
2025-03-24 16:46:22 +0200

    change logo path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Logo() {
   return (
     <div className="flex justify-center">
-      <img src="/assets/logo_dark.png" alt="Logo" />
+      <img
+        src={import.meta.env.BASE_URL + "/assets/logo_dark.png"}
+        alt="Logo"
+      />
     </div>
   );
 }
