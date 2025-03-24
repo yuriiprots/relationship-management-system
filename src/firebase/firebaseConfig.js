6cee7b8ca2a86430aa77bf4e67a342f29cb0fe82
yuriiprots <uraprots7849@gmail.com>
2025-03-24 16:55:09 +0200

    change authDomain

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "yuriiprots.github.io",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
