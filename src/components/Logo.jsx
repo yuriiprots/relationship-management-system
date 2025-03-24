dc577d3562f964d8d00dbc54e8cc20d76fe4e877
yuriiprots <uraprots7849@gmail.com>
2025-03-24 16:33:39 +0200

    try to deploy github page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Logo() {
   return (
     <div className="flex justify-center">
-      {/* <img src="/assets/logo.png" alt="Logo" /> */}
+      <img src="/assets/logo.png" alt="Logo" />
     </div>
   );
 }
