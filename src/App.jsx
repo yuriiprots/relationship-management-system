bc5ffdf6f14956fa43190ff437a8b50b483b6d89
yuriiprots <uraprots7849@gmail.com>
2025-03-24 15:54:56 +0200

    change browserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from "./contexts/authContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AppRouter from "./components/AppRouter";
